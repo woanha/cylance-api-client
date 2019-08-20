857cd21a395a33e2feb83626c7e478ba955503fc
Hahn Wolfgang Andreas <U586860@VOESTALPINE.root.local>
2019-08-20 13:02:18 +0200

    Added some tests

--- API_TestCalls.py
+++ API_TestCalls.py
@@ -10,12 +10,16 @@ Cylance = CyApiHandler(configFilePath = "CyApiConfig.txt")
 now = datetime.utcnow()
 startDate = now - timedelta(hours=6)
 
-df_detections = Cylance.GetDetectionsCSVList(startDate.strftime('%Y-%m-%dT%H:%M:%SZ'), now.strftime('%Y-%m-%dT%H:%M:%SZ'), detectionType="Internet Browser With Suspicious Parent")
+csv_detections = Cylance.GetDetectionsCSVList(startDate.strftime('%Y-%m-%dT%H:%M:%SZ'), now.strftime('%Y-%m-%dT%H:%M:%SZ'), detectionType="Internet Browser With Suspicious Parent")
+df_detections = Cylance.Csv2DataFrame(csv_detections)
 print df_detections
+#df_detections.to_csv('detections.csv')
 
-Cylance.getDetectionDetails(df_detections)
+#Cylance.getDetectionDetails(df_detections) #return a list of jsons?
+
+Cylance.deleteDetection("0815a5d0-feac-4187-89f3-84f407dce359")
 
 #time.sleep(5)
-detectionID = "97614e5e-1e5b-4de8-a89c-b5944b6d5cc8"
-detectionDetails = Cylance.getDetection(detectionID)
-print detectionDetails
+#detectionID = "97614e5e-1e5b-4de8-a89c-b5944b6d5cc8"
+#detectionDetails = Cylance.getDetection(detectionID)
+#print detectionDetails
